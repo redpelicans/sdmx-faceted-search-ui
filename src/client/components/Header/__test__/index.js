a670198d157afe0979e98ef5d750a8ad89a70246
luc skywallker <lucascharvolin@MacBook-Pro.local>
2017-07-31 10:28:38 +0200

    add test

---
+++ src/client/components/Header/__test__/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import should from 'should';
+
+const { descrive, it } = global;
+
+describe ('Fake', function () {
+  it('should add', function () {
+    should(1+1).eql(2);
+  })
+})
