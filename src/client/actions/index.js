d7c4d0e20ca8e9c5a46cac181bd6309980bd4cf0
lcharvol <lcharvol@student.42.fr>
2017-08-02 10:37:59 +0200

    fix errors

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,6 +1,6 @@
 export const SEARCH = 'SEARCH';
 export const FILTER = 'FILTER';
 export const FACETEDSEARCH = 'FACETEDSEARCH';
-export const Search = value => ({ type: SEARCH, value });
-export const Filter = value => ({ type: FILTER, value });
-export const FacetedSearch = value => ({ type: FACETEDSEARCH, value });
+export const search = value => ({ type: SEARCH, value });
+export const filter = value => ({ type: FILTER, value });
+export const facetedSearch = value => ({ type: FACETEDSEARCH, value });
--- src/client/components/App/index.js
+++ src/client/components/App/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import connect from '../../connect';
 import Container from '../Container';
 import SidePanel from '../SidePanel';
-import { Search, Filter, FacetedSearch } from '../../actions';
+import { search, filter, facetedSearch } from '../../actions';
 import './App.css';
 
 class App extends Component {
@@ -17,17 +17,17 @@ class App extends Component {
   }
 
   Search = ({ target: { value } }) => {
-    const { Search: srch } = this.props;
+    const { search: srch } = this.props;
     srch(value);
   };
 
   Filter = (value) => {
-    const { Filter: fltr } = this.props;
+    const { filter: fltr } = this.props;
     fltr(value);
   }
 
   FacetedSearch = (value) => {
-    const { FacetedSearch: fct } = this.props;
+    const { facetedSearch: fct } = this.props;
     fct(value);
   }
 
@@ -81,28 +81,28 @@ App.propTypes = {
   title: PropTypes.string.isRequired,
   language: PropTypes.array.isRequired,
   searchValue: PropTypes.string.isRequired,
-  Search: PropTypes.func.isRequired,
-  Filter: PropTypes.func.isRequired,
-  FacetedSearch: PropTypes.func.isRequired,
+  search: PropTypes.func.isRequired,
+  filter: PropTypes.func.isRequired,
+  facetedSearch: PropTypes.func.isRequired,
 };
 
 App.childContextTypes = {
   store: PropTypes.object.isRequired,
 };
 
-const filterList = (inilist, searchValue, filterValue, facetedValue) => {
-  let list = inilist;
-  if (facetedValue && facetedValue !== 'Tout') {
-    list = list.filter((li) => (li.Categories === facetedValue));
+const filterList = ({ list, searchValue, filterValue, facetedValue }) => {
+  let newList = list;
+  if (facetedValue && facetedValue !== 'All') {
+    newList = newList.filter((li) => (li.Categories === facetedValue));
   }
-  if (!searchValue && (!filterValue || filterValue === 'Tout')) {
-    return list;
-  } else if (searchValue && (!filterValue || filterValue === 'Tout')) {
-    return list.filter((li) => (li.Name.toLowerCase().match(searchValue.toLowerCase())));
+  if (!searchValue && (!filterValue || filterValue === 'All')) {
+    return newList;
+  } else if (searchValue && (!filterValue || filterValue === 'All')) {
+    return newList.filter((li) => (li.Name.toLowerCase().match(searchValue.toLowerCase())));
   } else if (filterValue && searchValue) {
-    return list.filter((li) => (li.Name.toLowerCase().match(searchValue.toLowerCase()) && filterValue === li.Type));
-  } else if (!searchValue && (filterValue && filterValue !== 'Tout')) {
-    return list.filter((li) => (filterValue === li.Type));
+    return newList.filter((li) => (li.Name.toLowerCase().match(searchValue.toLowerCase()) && filterValue === li.Type));
+  } else if (!searchValue && (filterValue && filterValue !== 'All')) {
+    return newList.filter((li) => (filterValue === li.Type));
   }
 };
 
@@ -111,15 +111,15 @@ const mapStateToProps = state => ({
   facetedbox: state.facetedbox,
   filterbox: state.filterbox,
   language: state.language,
-  list: filterList(state.list, state.searchValue, state.filterValue, state.facetedValue),
+  list: filterList(state),
   searchValue: state.searchValue,
   filterValue: state.filterValue,
   facetedValue: state.facetedValue,
 });
 
 const mapDispatchToProps = dispatch => ({
-  FacetedSearch: (value) => dispatch(FacetedSearch(value)),
-  Search: (value) => dispatch(Search(value)),
-  Filter: (value) => dispatch(Filter(value)),
+  facetedSearch: (value) => dispatch(facetedSearch(value)),
+  search: (value) => dispatch(search(value)),
+  filter: (value) => dispatch(filter(value)),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(App);
