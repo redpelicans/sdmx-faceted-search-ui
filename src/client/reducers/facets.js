136ba2468eaed75e4ad2464db194aeaf0c61686e
lcharvol <lcharvol@student.42.fr>
2017-08-17 17:11:28 +0200

    load Facets

--- src/client/reducers/facets.js
+++ src/client/reducers/facets.js
@@ -1,5 +1,9 @@
+import { CONFIGLOADED } from '../actions/config';
+
 const reducer = (state = {}, action) => {
   switch (action.type) {
+    case CONFIGLOADED:
+      return action.facets;
     default:
       return state;
   }
